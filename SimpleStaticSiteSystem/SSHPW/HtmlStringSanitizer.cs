8c776de68f46c064ca6add6fae82898ebe5cc094
Brian Haddad <brianhaddad@gmail.com>
2022-01-18 15:49:55 -0700

    Removed potential bug that isn't currently covered by tests.

--- SimpleStaticSiteSystem/SSHPW/HtmlStringSanitizer.cs
+++ SimpleStaticSiteSystem/SSHPW/HtmlStringSanitizer.cs
@@ -2,7 +2,7 @@
 {
     public class HtmlStringSanitizer
     {
-        public string Sanitize(string[] lines) => Sanitize(lines.Join(""));
+        public string Sanitize(string[] lines) => Sanitize(lines.Join(" "));
 
         public string Sanitize(string text)
         {
@@ -13,7 +13,7 @@
             return text;
         }
 
-        private string RemoveNewLines(string text) => text.ReplaceAll(Environment.NewLine, "");
+        private string RemoveNewLines(string text) => text.ReplaceAll(Environment.NewLine, " ");
         private string RemoveDoubleSpaces(string text) => text.ReplaceAll("  ", " ");
         private string ReplaceTabWithSpace(string text) => text.ReplaceAll("	", " ");
         private string CleanUpTags(string text)
